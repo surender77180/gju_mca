5348de517468ffa4b2d9eebdfc670bfbb8968b03
surender77180 <surender791999@gmail.com>
2023-04-11 21:29:43 +0530

    passEye

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-.frame{
-    
-}
+function passFunction(){
+    const pass = document.getElementById("showpass");
+    const iconEye = document.getElementById("iconEye");
+    if (pass.type === "password"){
+        pass.type = "text";
+        iconEye.classList.remove("fa-eye-slash")
+        iconEye.classList.add("fa-eye")
+    } else{
+        pass.type = "password";
+        iconEye.classList.remove("fa-eye")
+        iconEye.classList.add("fa-eye-slash")
+    }
+}       
